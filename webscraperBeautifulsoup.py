764a9c9a97a8c4fe75a1c67705106b98610c56a8
boruchk <boruk770@gmail.com>
2024-12-12 00:32:20 -0500

    added gitignore

--- webscraperBeautifulsoup.py
+++ webscraperBeautifulsoup.py
@@ -1,6 +1,6 @@
 from time import sleep
+import re
 import requests
-import lxml as lxmlThing
 from bs4 import BeautifulSoup
 
 # exponential backoff
@@ -10,10 +10,7 @@ month_urls = []
 day_urls = []
 changelog_urls = []
 
-keywords = ['return', 'port'] 
-# keywords = ['desktop', 'Desktop', 'DESKTOP', ' pc ', ' Pc ', ' PC ']
-urlFilePath = "changelogurls.txt"
-mentionFilePath = "daysMentioned.txt"
+keywords = ['desktop', 'Desktop', 'DESKTOP', ' pc ', ' Pc ', ' PC ']
 
 
 
@@ -36,48 +33,99 @@ def fetch_url_with_backoff(url, retryCount=5, backOffFactor=1):
       return None
 
 
-def search_urls(file, changelog_url):
+def get_year_urls():
+  file = open("urls\yearUrls.txt", 'a')
+  fetchedHtml = fetch_url_with_backoff(root_url)
+  if fetchedHtml is None:
+    print("Failed to fetch content from the URL.")
+    return
+  
+  soup = BeautifulSoup(fetchedHtml.text, features="xml")
+  for child in soup.findAll('a', class_="nb"):
+    if len(child.text) == 4:
+      file.write(f"https://lkml.org{child.get('href')}\n")
+
+  file.close()
+
+
+def get_month_urls(yearFile, year_url):
+  fetchedHtml = fetch_url_with_backoff(year_url)
+  if fetchedHtml is None:
+    print("Failed to fetch content from the URL.")
+    return
+  
+  soup = BeautifulSoup(fetchedHtml.text, features="xml")
+  for child in soup.findAll('a', class_="nb"):
+    if len(child.text) == 8:
+      yearFile.write(f"https://lkml.org{child.get('href')}\n")
+
+
+def get_day_urls(monthFile, month_url):
+  fetchedHtml = fetch_url_with_backoff(month_url)
+  if fetchedHtml is None:
+    print("Failed to fetch content from the URL.")
+    return
+  
+  soup = BeautifulSoup(fetchedHtml.text, features="xml")
+  for child in soup.findAll('a', class_="nb"):
+    if len(child.text) >= 10 and len(child.text) <= 11:
+      monthFile.write(f"https://lkml.org{child.get('href')}\n")
+
+
+def get_changelog_urls(changelogFile, day_url):
+  fetchedHtml = fetch_url_with_backoff(day_url)
+  if fetchedHtml is None:
+    print("Failed to fetch content from the URL.")
+    return
+  
+  soup = BeautifulSoup(fetchedHtml.text, features="xml")
+  for child in soup.findAll('a', class_="nb"):
+    text = child.get('href')
+    if len(child.get('href')) >= 16 and len(child.get('href')) <= 21:
+      changelogFile.write(f"https://lkml.org{child.get('href')}\n")
+
+
+def search_urls(changelog_url):
   fetchedHtml = fetch_url_with_backoff(changelog_url)
   if fetchedHtml is None:
     print("Failed to fetch content from the URL.")
     return
 
-  soup = BeautifulSoup(fetchedHtml, "lxml")
-  found = soup.findAll('pre', string=keywords)
-
-  date = changelog_url[-12:]
-  file.write(f"{date} - {found}")
-  file.write('\n')
+  soup = BeautifulSoup(fetchedHtml.text, features="xml")
+  result = soup.get_text()
+  date = changelog_url[-10:]
+  for word in keywords:
+    if word in result:
+      file.write(f"{date} - {word}")
+      file.write('\n')    
 
         
 
 # Start of program
 
-# response = requests.get('https://lkml.org/lkml/2020/5/5/7') 
+# get_year_urls()
+
+# yearFile = open("urls\yearUrls.txt", 'r')
+# for year_url in file:
+#   get_month_urls(yearFile, year_url.strip('\n'))
+# yearFile.close()
+
+# monthFile = open("urls\monthUrls.txt", 'r')
+# for month_url in file:
+#   get_day_urls(monthFile, month_url.strip('\n'))
+# monthFile.close()
+
+dayFile = open("urls\dayUrls.txt", 'r')
+changelogFile = open("urls\changelogurls.txt", 'a')
+for day_url in dayFile:
+  get_changelog_urls(changelogFile, day_url.strip('\n'))
+dayFile.close()
+changelogFile.close()
+
 urlFile = open(urlFilePath, 'r')
 mentionFile = open(mentionFilePath, 'w')
+for changelog_url in changelog_urls:
+  search_urls(mentionFile, changelog_url)
 
-search_urls(mentionFile, 'https://lkml.org/lkml/2020/5/5/7')
-# get_year_urls()
-# for year_url in year_urls:
-#   get_month_urls(year_url)
-
-# for month_url in month_urls:
-#   get_day_urls(month_url)
-
-# for day_url in day_urls:
-#   get_changelog_urls(day_url)
-  
-# test code
-# for i in range(3):
-#   get_day_urls(month_urls[i])
-# for i in range(1):
-#   get_changelog_urls(day_urls[i])
-
-# urlFile = open(urlFilePath, 'r')
-# mentionFile = open(mentionFilePath, 'w')
-# for changelog_url in changelog_urls:
-#   search_urls(mentionFile, changelog_url)
-
-urlFile.close()
-mentionFile.close()
+# urlFile.close()
+# mentionFile.close()
